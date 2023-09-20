fb32b1f7b29b88d9a1f1909f0f45cd54dd5b9ae6
daneeskripter <imdanee@seznam.cz>
2023-09-20 17:54:28 +0200

    socket io added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const colors = require("colors")
 const { exec } = require("child_process")
 const fs = require("fs")
 const jszip = require("jszip")
+const http = require("http").Server(app)
+const io = require('socket.io')(http)
 
 
 app.set("view-engine", "ejs")
@@ -15,20 +17,20 @@ app.get("/", function (req, res) {
     res.render(__dirname + "/views/index.ejs")
 })
 
-app.post("/", function (req, res) {
-    const url = req.body.url
-    const format = req.body.format
-    const command = `yt-dlp -x -i --audio-format ${format} -o "${__dirname + "/temp/%(title)s"}" --yes-playlist "${url}"`;
-    console.log(colors.green("[DOWNLOAD] ") + "Stahuju audio z youtube")
-    
-    exec(command, (error, stdout, stderr) => {
+app.get("/download", function (req, res) {
+    res.render(__dirname + "/views/download.ejs")
+    const url = req.query.url
+    const format = req.query.format
+    const command = `yt-dlp -x -i -v --audio-format ${format} -o "${__dirname + "/temp/%(title)s"}" --yes-playlist "${url}"`;
+    exec(command, function (error, stdout, stderr) {
         if (error) {
           console.error(`Chyba: ${error}`);
           return;
         }
         const num = Math.floor(Math.random() * 10000).toString()
         const zip = new jszip()
-        console.log(colors.yellow("[ARCHIVE] ") + `Archivuju audio soubory do souboru playlist-${num}.zip`)
+        io.emit("status", `Archiving audio files to playlist-${num}.zip`)
+        console.log(colors.yellow("[ARCHIVE] ") + `Archiving audio files to playlist-${num}.zip`)
         const folder = fs.readdirSync(__dirname + "/temp/")
         folder.forEach((file) => {
             zip.file(file, fs.readFileSync(__dirname + "/temp/" + file).buffer)    
@@ -37,8 +39,8 @@ app.post("/", function (req, res) {
         zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
         .pipe(fs.createWriteStream(__dirname + `/downloads/playlist-${num}.zip`))
         .on('finish', function () {
+            io.emit("status", `Archive playlist-${num}.zip was created. <a style="color: green;" href="/playlist-${num}.zip">Click to Download</a>`)
             console.log(colors.yellow("[ARCHIVE] ") + `Archiv playlist-${num}.zip byl vytvořen`)
-            res.redirect(`/playlist-${num}.zip`)
             folder.forEach((file) => {
             fs.rmSync(__dirname + `/temp/${file}`)
             })
@@ -48,6 +50,6 @@ app.post("/", function (req, res) {
 
 const port = 5000
 
-app.listen(port, function () {
+http.listen(port, function () {
     console.log(colors.green("[SERVER] ") + `Server běží na portu ${port}`)
 })
